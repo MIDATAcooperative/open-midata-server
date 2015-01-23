b37327350342d6e8eb12b5a33ebeb2f4f9d66ef2
Alexander Kreutz <kreutz@i-tv-t.de>
2015-01-23 17:54:07 +0100

    added front end pages

---
+++ platform/app/controllers/members/MemberFrontend.java
@@ -0,0 +1,32 @@
+package controllers.members;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.members.configuration;
+import views.html.members.providers;
+import views.html.members.studies;
+import views.html.members.studydetails;
+import views.html.members.studyrecords;
+
+public class MemberFrontend extends Controller {
+
+	public static Result configuration() {
+		return ok(configuration.render());
+	}
+	
+	public static Result providers() {
+		return ok(providers.render());
+	}
+	
+	public static Result studies() {
+		return ok(studies.render());
+	}
+	
+	public static Result studydetails(String study) {
+		return ok(studydetails.render());
+	}
+	
+	public static Result studyrecords(String study) {
+		return ok(studyrecords.render());
+	}
+}
--- platform/app/controllers/research/ResearchFrontend.java
+++ platform/app/controllers/research/ResearchFrontend.java
@@ -6,6 +6,15 @@ import play.mvc.Security;
 import views.html.research.registration;
 import views.html.research.login;
 import views.html.research.messages;
+import views.html.research.codes;
+import views.html.research.createstudy;
+import views.html.research.studyfields;
+import views.html.research.studymessages;
+import views.html.research.studyoverview;
+import views.html.research.studyparticipants;
+import views.html.research.studyrecords;
+import views.html.research.studyresults;
+import views.html.research.studyrules;
 
 public class ResearchFrontend extends Controller {
 
@@ -20,5 +29,41 @@ public class ResearchFrontend extends Controller {
 	@Security.Authenticated(ResearchSecured.class)
 	public static Result messages() {
 		return ok(messages.render());
+	}
+	
+	public static Result codes(String study) {
+		return ok(codes.render());
+	}
+	
+	public static Result createstudy() {
+		return ok(createstudy.render());
+	}
+	
+	public static Result studyfields(String study) {
+		return ok(studyfields.render());
+	}
+	
+	public static Result studymessages(String study) {
+		return ok(studymessages.render());
+	}
+	
+	public static Result studyoverview(String study) {
+		return ok(studyoverview.render());
+	}
+	
+	public static Result studyparticipants(String study) {
+		return ok(studyparticipants.render());
+	}
+	
+	public static Result studyrecords(String study) {
+		return ok(studyrecords.render());
+	}
+	
+	public static Result studyresults(String study) {
+		return ok(studyresults.render());
+	}
+	
+	public static Result studyrules(String study) {
+		return ok(studyresults.render());
 	}
 }
