4e636b073a959dbf63674a2e41cc28bab208750c
Alexander Kreutz <kreutz@i-tv-t.de>
2015-11-19 16:55:37 +0100

    MIDATA name

--- portal/src/assets/services/studies.js
+++ portal/src/assets/services/studies.js
@@ -4,7 +4,7 @@ angular.module('services')
 	
 	service.search = function(properties, fields) {
 		var data = {"properties": properties, "fields": fields };
-		return server.post(jsRoutes.controllers.common.Studies.search().url, JSON.stringify(data));
+		return server.post(jsRoutes.controllers.Studies.search().url, JSON.stringify(data));
 	};
 	
 	service.updateParticipation = function(studyId, data) {
