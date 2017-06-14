03ceba99f7222e6a53862aa9f16e820d2c9484a6
Alexander Kreutz <kreutz@i-tv-t.de>
2017-06-14 09:25:48 +0200

    questionnaire improvements

--- visualizations/fhir-surveys/src/i18n.js
+++ visualizations/fhir-surveys/src/i18n.js
@@ -2,7 +2,10 @@ var en = {
   "next" : "Next",
   "skip" : "Skip Question",
   "back" : "Back",
-  "cancel" : "Cancel"
+  "cancel" : "Cancel",
+  "select_survey" : "Select a survey",
+  "last_answered" : "Last Answered",
+  "empty" : "This visualizations allows you to participate in surveys shared with you. You currently do not have any survey to participate."
 },
 de = {
 	
