d014f786d807ef432bac34f419ec054a1f2400bb
Alexander Kreutz <kreutz@i-tv-t.de>
2017-08-11 14:00:29 +0200

    fixes to birthday

--- portal/src/views/members/public/quickregistration/quickregistration.js
+++ portal/src/views/members/public/quickregistration/quickregistration.js
@@ -15,6 +15,12 @@ angular.module('portal')
 		
 		$scope.submitted = true;	
 		if ($scope.error && $scope.error.field && $scope.error.type) $scope.myform[$scope.error.field].$setValidity($scope.error.type, true);
+		
+	    $scope.myform.birthday.$setValidity('day', $scope.registration.birthdayDay > 0 && $scope.registration.birthdayDay < 32);
+        $scope.myform.birthday.$setValidity('month', $scope.registration.birthdayMonth > 0);
+		$scope.myform.birthday.$setValidity('year', $scope.registration.birthdayYear >= 1900 && $scope.registration.birthdayYear <= new Date().getFullYear());
+	
+		
 		$scope.error = null;
 		if (! $scope.myform.$valid) {
 			var elem = $document[0].querySelector('input.ng-invalid');
@@ -27,10 +33,14 @@ angular.module('portal')
 			return;
 		}
 		        
-        $scope.registration.birthday = $scope.registration.birthdayYear + "-" + 
-                                       $scope.registration.birthdayMonth + "-" +
-                                       $scope.registration.birthdayDay;
+        var pad = function(n){
+		    return ("0" + n).slice(-2);
+		};
 		
+        $scope.registration.birthday = $scope.registration.birthdayYear + "-" + 
+                                       pad($scope.registration.birthdayMonth) + "-" +
+                                       pad($scope.registration.birthdayDay);		
+	
 		// send the request
 		var data = $scope.registration;		
 		$scope.status.doAction("register", server.post(jsRoutes.controllers.QuickRegistration.register().url, JSON.stringify(data))).
