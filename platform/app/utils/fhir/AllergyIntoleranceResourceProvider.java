6075bff59c1e5de962b598201f135d62088b659b
Alexander Kreutz <kreutz@i-tv-t.de>
2018-01-02 15:36:49 +0100

    added AllergyIntolerance resource

---
+++ platform/app/utils/fhir/AllergyIntoleranceResourceProvider.java
@@ -0,0 +1,244 @@
+package utils.fhir;
+
+import java.util.List;
+import java.util.Set;
+
+import org.hl7.fhir.dstu3.model.AllergyIntolerance;
+import org.hl7.fhir.dstu3.model.IdType;
+import org.hl7.fhir.dstu3.model.Reference;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+import ca.uhn.fhir.model.api.Include;
+import ca.uhn.fhir.model.api.annotation.Description;
+import ca.uhn.fhir.rest.annotation.Create;
+import ca.uhn.fhir.rest.annotation.IdParam;
+import ca.uhn.fhir.rest.annotation.IncludeParam;
+import ca.uhn.fhir.rest.annotation.OptionalParam;
+import ca.uhn.fhir.rest.annotation.ResourceParam;
+import ca.uhn.fhir.rest.annotation.Search;
+import ca.uhn.fhir.rest.annotation.Sort;
+import ca.uhn.fhir.rest.annotation.Update;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.api.SortSpec;
+import ca.uhn.fhir.rest.param.DateAndListParam;
+import ca.uhn.fhir.rest.param.DateRangeParam;
+import ca.uhn.fhir.rest.param.ReferenceAndListParam;
+import ca.uhn.fhir.rest.param.StringAndListParam;
+import ca.uhn.fhir.rest.param.TokenAndListParam;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import models.Record;
+import utils.auth.ExecutionInfo;
+import utils.collections.Sets;
+import utils.exceptions.AppException;
+
+public class AllergyIntoleranceResourceProvider extends ResourceProvider<AllergyIntolerance> implements IResourceProvider {
+
+	public AllergyIntoleranceResourceProvider() {
+		searchParamNameToPathMap.put("AllergyIntolerance:asserter", "asserter");
+		searchParamNameToTypeMap.put("AllergyIntolerance:asserter", Sets.create("Practitioner", "Patient", "RelatedPerson"));
+		searchParamNameToPathMap.put("AllergyIntolerance:recorder", "recorder");
+		searchParamNameToTypeMap.put("AllergyIntolerance:recorder", Sets.create("Practitioner", "Patient", "RelatedPerson"));		
+		searchParamNameToPathMap.put("AllergyIntolerance:patient", "patient");
+		searchParamNameToTypeMap.put("AllergyIntolerance:patient", Sets.create("Patient"));
+		
+		registerSearches("AllergyIntolerance", getClass(), "getAllergyIntolerance");
+	}
+	
+	@Override
+	public Class<AllergyIntolerance> getResourceType() {
+		return AllergyIntolerance.class;
+	}
+
+	@Search()
+	public List<IBaseResource> getAllergyIntolerance(
+			@Description(shortDefinition = "The resource identity") @OptionalParam(name = "_id") StringAndListParam theId,
+
+			@Description(shortDefinition = "The resource language") @OptionalParam(name = "_language") StringAndListParam theResourceLanguage,
+
+		
+  			@Description(shortDefinition="Source of the information about the allergy")
+  			@OptionalParam(name="asserter", targetTypes={  } )
+  			ReferenceAndListParam theAsserter, 
+    
+  			@Description(shortDefinition="food | medication | environment | biologic")
+  			@OptionalParam(name="category")
+  			TokenAndListParam theCategory, 
+    
+  			@Description(shortDefinition="active | inactive | resolved")
+  			@OptionalParam(name="clinical-status")
+  			TokenAndListParam theClinical_status, 
+    
+  			@Description(shortDefinition="Code that identifies the allergy or intolerance")
+  			@OptionalParam(name="code")
+  			TokenAndListParam theCode, 
+    
+  			@Description(shortDefinition="low | high | unable-to-assess")
+  			@OptionalParam(name="criticality")
+  			TokenAndListParam theCriticality, 
+    
+  			@Description(shortDefinition="Date record was believed accurate")
+  			@OptionalParam(name="date")
+			DateAndListParam theDate, 
+    
+  			@Description(shortDefinition="External ids for this item")
+  			@OptionalParam(name="identifier")
+  			TokenAndListParam theIdentifier, 
+    
+  			@Description(shortDefinition="Date(/time) of last known occurrence of a reaction")
+  			@OptionalParam(name="last-date")
+			DateAndListParam theLast_date, 
+    
+  			@Description(shortDefinition="Clinical symptoms/signs associated with the Event")
+  			@OptionalParam(name="manifestation")
+ 			TokenAndListParam theManifestation, 
+   
+ 			@Description(shortDefinition="Date(/time) when manifestations showed")
+ 			@OptionalParam(name="onset")
+			DateAndListParam theOnset, 
+   
+ 			@Description(shortDefinition="Who the sensitivity is for")
+ 			@OptionalParam(name="patient", targetTypes={  } )
+ 			ReferenceAndListParam thePatient, 
+   
+ 			@Description(shortDefinition="Who recorded the sensitivity")
+ 			@OptionalParam(name="recorder", targetTypes={  } )
+ 			ReferenceAndListParam theRecorder, 
+   
+ 			@Description(shortDefinition="How the subject was exposed to the substance")
+ 			@OptionalParam(name="route")
+ 			TokenAndListParam theRoute, 
+   
+ 			@Description(shortDefinition="mild | moderate | severe (of event as a whole)")
+ 			@OptionalParam(name="severity")
+ 			TokenAndListParam theSeverity, 
+   
+ 			@Description(shortDefinition="allergy | intolerance - Underlying mechanism (if known)")
+ 			@OptionalParam(name="type")
+ 			TokenAndListParam theType, 
+   
+ 			@Description(shortDefinition="unconfirmed | confirmed | refuted | entered-in-error")
+ 			@OptionalParam(name="verification-status")
+ 			TokenAndListParam theVerification_status,   		
+ 
+ 			@IncludeParam(reverse=true)
+ 			Set<Include> theRevIncludes,
+ 			@Description(shortDefinition="Only return resources which were last updated as specified by the given range")
+ 			@OptionalParam(name="_lastUpdated")
+ 			DateRangeParam theLastUpdated, 
+ 
+ 			@IncludeParam(allow= {
+ 					"AllergyIntolerance:asserter" ,
+ 					"AllergyIntolerance:patient" ,
+ 					"AllergyIntolerance:recorder" ,
+ 					"*"
+ 			}) 
+ 			Set<Include> theIncludes,
+ 			 						 		
+			@Sort 
+			SortSpec theSort,
+			 			
+			@ca.uhn.fhir.rest.annotation.Count
+			Integer theCount
+
+	) throws AppException {
+
+		SearchParameterMap paramMap = new SearchParameterMap();
+
+		paramMap.add("_id", theId);
+		paramMap.add("_language", theResourceLanguage);
+	
+		paramMap.add("asserter", theAsserter);
+		paramMap.add("category", theCategory);
+		paramMap.add("clinical-status", theClinical_status);
+		paramMap.add("code", theCode);
+		paramMap.add("criticality", theCriticality);
+		paramMap.add("date", theDate);
+		paramMap.add("identifier", theIdentifier);
+		paramMap.add("last-date", theLast_date);
+		paramMap.add("manifestation", theManifestation);
+		paramMap.add("onset", theOnset);
+		paramMap.add("patient", thePatient);
+		paramMap.add("recorder", theRecorder);
+		paramMap.add("route", theRoute);
+		paramMap.add("severity", theSeverity);
+		paramMap.add("type", theType);
+		paramMap.add("verification-status", theVerification_status);		
+		
+		
+		paramMap.setRevIncludes(theRevIncludes);
+		paramMap.setLastUpdated(theLastUpdated);
+		paramMap.setIncludes(theIncludes);
+		paramMap.setSort(theSort);
+		paramMap.setCount(theCount);
+
+		return search(paramMap);
+	}
+
+	public List<Record> searchRaw(SearchParameterMap params) throws AppException {
+		ExecutionInfo info = info();
+
+		Query query = new Query();		
+		QueryBuilder builder = new QueryBuilder(params, query, "fhir/AllergyIntolerance");
+
+		builder.handleIdRestriction();
+		builder.recordOwnerReference("patient", "Patient", "patient");
+
+		builder.restriction("identifier", true, QueryBuilder.TYPE_IDENTIFIER, "identifier");
+		builder.restriction("code", true, QueryBuilder.TYPE_CODEABLE_CONCEPT, "code", QueryBuilder.TYPE_CODEABLE_CONCEPT, "reaction.substance");
+		builder.restriction("date", true, QueryBuilder.TYPE_DATETIME, "assertedDate");
+		
+		builder.restriction("asserter", true, null, "asserter");				        
+		builder.restriction("last-date", true, QueryBuilder.TYPE_DATETIME, "lastOccurrence");			
+		builder.restriction("manifestation", true, QueryBuilder.TYPE_CODEABLE_CONCEPT, "reaction.manifestation");
+		builder.restriction("onset", true, QueryBuilder.TYPE_DATETIME, "reaction.onset");	
+		builder.restriction("recorder", true, null, "recorder");	
+		builder.restriction("route", true, QueryBuilder.TYPE_CODEABLE_CONCEPT, "reaction.exposureRoute");	
+		builder.restriction("severity", false, QueryBuilder.TYPE_CODE, "reaction.severity");
+		builder.restriction("type", false, QueryBuilder.TYPE_CODE, "type");
+		builder.restriction("criticality", false, QueryBuilder.TYPE_CODE, "criticality");
+		builder.restriction("category", false, QueryBuilder.TYPE_CODE, "category");	
+		builder.restriction("clinical-status", false, QueryBuilder.TYPE_CODE, "clinicalStatus");			
+		builder.restriction("verification-status", false, QueryBuilder.TYPE_CODE, "verificationStatus");	
+								
+		return query.execute(info);
+	}
+
+	@Create
+	@Override
+	public MethodOutcome createResource(@ResourceParam AllergyIntolerance theAllergyIntolerance) {
+		return super.createResource(theAllergyIntolerance);
+	}
+			
+	public Record init() { return newRecord("fhir/AllergyIntolerance"); }
+
+	@Update
+	@Override
+	public MethodOutcome updateResource(@IdParam IdType theId, @ResourceParam AllergyIntolerance theAllergyIntolerance) {
+		return super.updateResource(theId, theAllergyIntolerance);
+	}		
+
+	public void prepare(Record record, AllergyIntolerance theAllergyIntolerance) throws AppException {
+		// Set Record code and content
+		record.name = setRecordCodeByCodings(record, null, "AllergyIntolerance");		
+						
+		if (record.name==null) record.name = "Allergy Intolerance";		
+
+		// clean
+		Reference subjectRef = theAllergyIntolerance.getPatient();
+		if (cleanAndSetRecordOwner(record, subjectRef)) theAllergyIntolerance.setPatient(null);
+		
+		clean(theAllergyIntolerance);
+ 
+	}	
+ 
+	@Override
+	public void processResource(Record record, AllergyIntolerance p) throws AppException {
+		super.processResource(record, p);
+		
+		if (p.getPatient().isEmpty()) {			
+			p.setPatient(FHIRTools.getReferenceToUser(record.owner, record.ownerName));
+		}
+	}
+	
+
+}
