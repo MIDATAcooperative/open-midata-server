52b34ac55ac93671f965e65a31b10c4b087d6330
Dick Chavez <dickpaul.chavezarias@bfh.ch>
2018-09-19 00:55:05 +0200

    WIP

--- portal/src/main-portal.js
+++ portal/src/main-portal.js
@@ -1,1 +1,20 @@
-require('./config');
+var glob = require('glob');
+var path = require('path');
+require('angular');
+//require('./app')
+//require('./config');
+
+//autoload('./assets/**/*.js');
+//autoload('./views/**/*.js');
+
+//glob.sync( './src/assets/**/*.js' ).forEach( function( file ) {
+//    console.log('added: ' + path.resolve( file ));
+//    require( path.resolve( file ) );
+//  });
+//glob.sync( './views/**/*.js' ).forEach( function( file ) {
+//    console.log('added: ' + path.resolve( file ));
+//    require( path.resolve( file ) );
+//  });
+
+require.context('./assets', true, /\.js$/);
+require.context('./views', true, /\.js$/);
