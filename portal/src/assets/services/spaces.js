220bb7933d0651c91852008cedf8f427ae448c77
Alexander Kreutz <kreutz@i-tv-t.de>
2018-09-21 14:57:52 +0200

    fix inter plugin links on first use of plugin

--- portal/src/assets/services/spaces.js
+++ portal/src/assets/services/spaces.js
@@ -97,7 +97,7 @@ angular.module('services')
 									  if (app.type === "oauth1" || app.type === "oauth2") {
 										 $state.go("^.importrecords", { "spaceId" : result.data._id, params : JSON.stringify(data.params) });
 									  } else { 
-									     $state.go('^.spaces', { spaceId : result.data._id, user : data.user });
+									     $state.go('^.spaces', { spaceId : result.data._id, params : JSON.stringify(data.params), user : data.user });
 									  }
 									} else {
 									  $state.go('^.dashboard', { dashId : $scope.options.context });
