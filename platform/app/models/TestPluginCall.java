efd2dad9a1a3f4c444edfc78393963869835016b
Alexander Kreutz <kreutz@i-tv-t.de>
2019-01-29 11:04:55 +0100

    fixes for answering FHIR messages while debugging

--- platform/app/models/TestPluginCall.java
+++ platform/app/models/TestPluginCall.java
@@ -15,10 +15,11 @@ public class TestPluginCall extends Model {
 
 	protected @NotMaterialized static final String collection = "testcalls";
 	
+	public @NotMaterialized final static int NOTANSWERED = -999;
+	
 	public @NotMaterialized final static Set<String> ALL = 
-			 Sets.create("_id", "handle", "path", "token", "lang", "owner", "resource", "resourceId", "created", "returnPath");
-
-	public String returnPath;
+			 Sets.create("_id", "handle", "path", "token", "lang", "owner", "resource", "resourceId", "created", "answer", "answerStatus");
+	
 	
 	public String handle;
 	
@@ -34,6 +35,10 @@ public class TestPluginCall extends Model {
 	
 	public String resourceId;
 	
+	public String answer;
+	
+	public int answerStatus;
+	
 	public long created;
 	
 	public void add() throws InternalServerException {
@@ -41,10 +46,28 @@ public class TestPluginCall extends Model {
 	}
 	
 	public static List<TestPluginCall> getForHandle(String handle) throws InternalServerException {
-		return Model.getAllList(TestPluginCall.class, collection, CMaps.map("handle", handle), ALL, 1000, "created", 1);
+		return Model.getAllList(TestPluginCall.class, collection, CMaps.map("handle", handle).map("answerStatus", NOTANSWERED), ALL, 1000, "created", 1);
+	}
+	
+	public static TestPluginCall getForHandleAndId(String handle, MidataId id) throws InternalServerException {
+		return Model.get(TestPluginCall.class, collection, CMaps.map("_id",id).map("handle", handle), ALL);
+	}
+	
+	public static TestPluginCall getById(MidataId id) throws InternalServerException {
+		return Model.get(TestPluginCall.class, collection, CMaps.map("_id",id), ALL);
 	}
 	
 	public static void delete(String handle) throws InternalServerException {
 		Model.delete(TestPluginCall.class, collection, CMaps.map("handle", handle));
+	}
+	
+	public static void delete(String handle, MidataId id) throws InternalServerException {
+		Model.delete(TestPluginCall.class, collection, CMaps.map("_id",id).map("handle", handle));
+	}
+	
+	public void setAnswer(int status, String content) throws InternalServerException {
+		this.answerStatus = status;
+		this.answer = content;
+		this.setMultiple(collection, Sets.create("answerStatus", "answer"));
 	}
 }
