3613e4548b64e4504c9da467ff214a68e17ebf74
Alexander Kreutz <kreutz@i-tv-t.de>
2019-11-12 11:45:35 +0100

    role specific terms and conditions in footer

--- portal/src/views/shared/public/terms/config.js
+++ portal/src/views/shared/public/terms/config.js
@@ -3,23 +3,43 @@ angular.module('portal')
 	   $stateProvider
 	    .state('public.terms', {
 	      url: '/terms/:which?lang',
-	      templateUrl: 'views/shared/public/terms/terms.html',	      
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "member"
+	    })
+	    .state('public_provider.terms', {
+	      url: '/terms/:which?lang',
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "provider"
+	    })
+	    .state('public_research.terms', {
+	      url: '/terms/:which?lang',
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "research"
+	    })
+	    .state('public_developer.terms', {
+	      url: '/terms/:which?lang',
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "developer"
 	    })
 	    .state('member.terms', {
 	      url: '/terms/:which?lang',
-	      templateUrl: 'views/shared/public/terms/terms.html',	      
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "member"	      
 	    })
 	    .state('developer.terms', {
 	      url: '/terms/:which?lang',
-	      templateUrl: 'views/shared/public/terms/terms.html',	      
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "developer"	      
 	    })
 	    .state('admin.terms', {
 	      url: '/terms/:which?lang',
-	      templateUrl: 'views/shared/public/terms/terms.html',	      
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "admin"	      
 	    })
 	    .state('research.terms', {
 	      url: '/terms/:which?lang',
-	      templateUrl: 'views/shared/public/terms/terms.html',	      
+	      templateUrl: 'views/shared/public/terms/terms.html',
+	      termsRole : "research"	      
 	    });	    
 	   
 }]);
