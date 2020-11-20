33b318fd2e696bdf4435e4ba3a32bfebab16122d
Alexander Kreutz <kreutz@i-tv-t.de>
2020-11-20 15:30:23 +0100

    Open MIDATA Server

--- platform/app/utils/messaging/InputStreamCollector.java
+++ platform/app/utils/messaging/InputStreamCollector.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of the Open MIDATA Platform.
+ * This file is part of the Open MIDATA Server.
  *
- * The Open MIDATA platform is free software: you can redistribute it and/or modify
+ * The Open MIDATA Server is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * The Open MIDATA Platform is distributed in the hope that it will be useful,
+ * The Open MIDATA Server is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with the Open MIDATA platform.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the Open MIDATA Server.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package utils.messaging;
