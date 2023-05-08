efc900fa4c07dff27cefc00960f3cb61390d569e
Alexander Kreutz <kreutz@i-tv-t.de>
2023-05-08 16:36:58 +0200

    remove slashes

--- platform/app/controllers/FHIR.java
+++ platform/app/controllers/FHIR.java
@@ -194,6 +194,7 @@ public class FHIR extends Controller {
   		  
 		   if (serial!=null) {
 			   AccessLog.log("ser="+serial+";");
+			   serial = serial.replaceAll("\\", "");
 			   String[] serial2 = serial.split(",");
 			   MidataId instance = null;
 			   String key = null;
